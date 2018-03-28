f521afee8bda03a358a21a801dfb99f0f1e1ed91
claudiu9379 <office@bestdeveloper.ro>
2018-03-28 11:39:42 +0300

    sed 16

--- src/app/ui/user/login/login.component.ts
+++ src/app/ui/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {User} from './user.interface';
+import {HttpWrapperService} from "../../../services/http/httpService";
 
 
 @Component({
@@ -8,14 +9,32 @@ import {User} from './user.interface';
   styleUrls: ['login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public user: User;
+  // public user: User;
 
-
-  ngOnInit() {
-  this.user = {
+  user : User = {
     email:'',
     password:''
+  };
+
+
+  constructor(private httpService: HttpWrapperService){
   }
+
+  serverData : any = null;
+
+  async onSubmit(){
+    var request = {
+      login: this.user.email,
+      password: this.user.password
+    };
+
+    this.serverData = await this.httpService.postJson("api/pub/security/login",request);
+
+    console.log(this.serverData);
+  }
+
+  ngOnInit() {
+
   }
 }
 
