262d4b6db40680c0eef84106bca50241fb3d6c43
DESKTOP-A5NANPH\Catalin <catalin.petru@yahoo.ro>
2018-03-29 18:39:21 +0300

    Reset

--- src/app/ui/user/login/login.component.ts
+++ src/app/ui/user/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit} from '@angular/core';
-import {User} from './user.interface';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.interface';
+import { HttpWrapperService } from '../../../services/http/httpService';
+
+
 
 
 @Component({
@@ -8,14 +11,31 @@ import {User} from './user.interface';
   styleUrls: ['login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  constructor(private httpService: HttpWrapperService) {
+
+  }
+
+
+
+  async onSubmit(){
+    var request = {
+      login:this.user.email,
+      password:this.user.password
+    };
+    const serverResponse = await this.httpService.postJson('api/pub/security/login',request);
+
+    console.log(serverResponse);
+  }
+
   public user: User;
 
 
   ngOnInit() {
-  this.user = {
-    email:'',
-    password:''
-  }
+    this.user = {
+      email: '',
+      password: ''
+    }
   }
 }
 
